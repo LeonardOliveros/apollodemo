dc8ebcdad652b0349174108a2718409a83d0ae12
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2016-10-18 19:04:37 -0400

    Wrap up Your Client

---
+++ imports/ui/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PostsContainer from './postsContainer';
+
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return {
+			<PostsContainer />
+		}
+	}
+}
