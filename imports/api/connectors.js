168055efafa873029bdfbf8d6372c02fd2f93328
Leonardo Oliveros <leonardoliveros.lo@gmail.com>
2016-10-18 14:31:56 -0400

    Implement the Database Connectors

---
+++ imports/api/connectors.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+//Crear conexion
+const db = new Sequelize('apollodemo', "root", null, {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+
+//Definir el modelo
+const PostModel = db.define('post', {
+	content: { type: Sequelize.STRING },
+	views: { type: Sequelize.INTEGER }
+}, {
+	timestamps: false
+});
+
+//Crear la tabla si no existe
+db.sync();
+
+//Exportar Post
+const Post = db.models.post;
+export { Post }
